6369626427776c87d8d532e12a51ad5ec1df3d6b
Andrei Dodu <andrei.dodu@hotmail.it>
2023-12-04 22:36:02 +0100

    chore: configuration sample

--- src/config.sample.rs
+++ src/config.sample.rs
@@ -1,4 +1,8 @@
-const WIFI_SSID: &str = "";
-const WIFI_PASS: &str = "";
+pub const WIFI_SSID: &str = "";
+pub const WIFI_PASS: &str = "";
+// should be retrieved from server
+// at 8:45
 pub const DEFAULT_CRONTAB: &str =
-    "0-59   0-59   0-23     1-31       Jan-Dec  Mon,Tue,Wed,Thu,Fri,Sat,Sun  2023-2100";
+    "0   45   8     1-31       Jan-Dec  Mon,Tue,Wed,Thu,Fri  2023-2100";
+// should be retrieved from server
+pub const ALARM_INTERVAL_SECONDS: u32 = 1 * 60;
